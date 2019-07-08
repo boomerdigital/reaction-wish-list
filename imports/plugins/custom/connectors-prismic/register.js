93106260a53ed45046ef53a85c74d3c95dd3ce1f
jacques mouette <j.mouette@gmail.com>
2019-07-08 19:17:56 -0300

    Remove unnecessary props

--- imports/plugins/custom/connectors-prismic/register.js
+++ imports/plugins/custom/connectors-prismic/register.js
@@ -3,23 +3,5 @@ import Reaction from "/imports/plugins/core/core/server/Reaction";
 Reaction.registerPackage({
   label: "Prismic Connect",
   name: "reaction-connectors-prismic",
-  icon: "fa fa-exchange",
   autoEnable: true,
-  settings: {
-    apiKey: "",
-    password: "",
-    sharedSecret: "",
-    shopName: "",
-    webhooks: [],
-    webhooksDomain: ""
-  },
-  registry: [{
-    label: "prismic Connect Settings",
-    name: "settings/connectors/prismic",
-    icon: "fa fa-exchange",
-    route: "/dashboard/connectors/prismic",
-    provides: ["connectorSettings"],
-    container: "dashboard",
-    template: "prismicConnectSettings"
-  }]
 });
