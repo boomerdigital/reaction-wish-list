fa5bb1e3d8208f3410fc21382ce4e488056f2b1a
Allison Reilly <acreilly3@gmail.com>
2019-07-16 12:11:08 -0300

    Set up mutation to help update account information

---
+++ imports/plugins/core/accounts/server/no-meteor/mutations/updateAccount.js
@@ -0,0 +1,26 @@
+import ReactionError from "@reactioncommerce/reaction-error";
+
+
+export default async function updateAccount(context, input, accountUserId) {
+  const { collections, userHasPermission, userId: userIdFromContext } = context;
+  const { Accounts } = collections;
+  const { firstName, lastName, birthDate } = input;
+  const userId = accountUserId || userIdFromContext;
+  const account = await Accounts.findOne({ userId });
+
+  if (!account) throw new ReactionError("not-found", "No account found");
+
+  const { value: updatedAccount } = await Accounts.findOneAndUpdate(
+    { userId },
+    {
+    $set: {
+      firstName, lastName, birthDate
+    }
+  );
+
+  if (!updatedAccount) {
+    throw new ReactionError("server-error", "Unable to add address to account");
+  }
+
+  return updatedAccount;
+}
---
+++ imports/plugins/core/accounts/server/no-meteor/resolvers/Mutation/updateAccount.js
@@ -0,0 +1,29 @@
+import { decodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/shop";
+import { decodeAccountOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/account";
+
+export default async function updateAccount(parentResult, { input }, context) {
+  const {
+    clientMutationId = null,
+    id: opaqueAccountId,
+    shopId: opaqueShopId,
+    firstName,
+    lastName,
+    birthDate
+  } = input;
+
+  const shopId = decodeShopOpaqueId(opaqueShopId);
+  const accountId = decodeAccountOpaqueId(opaqueAccountId);
+
+  const account = await context.mutations.updateAccount(context, {
+    shopId,
+    accountId,
+    firstName,
+    lastName,
+    birthDate
+  });
+
+  return {
+    clientMutationId,
+    account
+  };
+}
