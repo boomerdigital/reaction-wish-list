09c10874389262499c42ba8789404974041cfaa0
jacques mouette <j.mouette@gmail.com>
2019-07-23 15:05:22 -0300

    Fix input expected structure

--- imports/plugins/core/accounts/server/no-meteor/resolvers/Mutation/updateAccountEntry.js
+++ imports/plugins/core/accounts/server/no-meteor/resolvers/Mutation/updateAccountEntry.js
@@ -1,21 +1,20 @@
-import { decodeShopOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/shop";
+/* eslint-disable require-jsdoc */
 import { decodeAccountOpaqueId } from "@reactioncommerce/reaction-graphql-xforms/account";
 
 export default async function updateAccountEntry(parentResult, { input }, context) {
   const {
     clientMutationId = null,
-    id: opaqueAccountId,
-    shopId: opaqueShopId,
-    firstName,
-    lastName,
-    birthDate
+    accountId: opaqueAccountId,
+    updates: {
+      firstName,
+      lastName,
+      birthDate
+    }
   } = input;
 
-  const shopId = decodeShopOpaqueId(opaqueShopId);
   const accountId = decodeAccountOpaqueId(opaqueAccountId);
 
   const account = await context.mutations.updateAccount(context, {
-    shopId,
     accountId,
     firstName,
     lastName,
